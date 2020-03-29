d2de6442912101bd0c2dd2bfa8f5aee20f565cc8
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-03-29 18:34:20 -0300

    Estrutura com ejs

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const port = 3333;
 const app = express();
+const port = 3333;
+const path = require('path');
+
+//Rotas instanciadas
+const perfilRouter = require('./routes/perfilRouter');
+
+//view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-    res.send({
-        title:"Projeto Integrador",
-        version:"0.0.0"
+    res.render('index', {
+        title: "Projeto Integrador MobMap",
+        version: "0.0.0"
     });
 });
 
+app.use('/perfil', perfilRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
 app.listen(port, err => {
-    console.log(`Servidor rodando na porta ${port}` );
+    console.log(`Servidor rodando na porta ${port}`);
 });
